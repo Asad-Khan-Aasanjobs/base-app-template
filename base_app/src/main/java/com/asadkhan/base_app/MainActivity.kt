90d5657fc535d707e640350f763f10096eca1ab5
AsadK <asad.khan@aasaanjobs.com>
2019-04-03 19:41:54 +0530

    Create some sort of binding between App & Library

--- base_app/src/main/java/com/asadkhan/base_app/MainActivity.kt
+++ base_app/src/main/java/com/asadkhan/base_app/MainActivity.kt
@@ -3,6 +3,7 @@ package com.asadkhan.base_app
 import android.app.Activity
 import android.os.Bundle
 import android.os.Handler
+import com.asadkhan.global.Constants
 import kotlinx.android.synthetic.main.activity_main.tv_hello_world
 
 class MainActivity : Activity() {
@@ -11,7 +12,8 @@ class MainActivity : Activity() {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
     Handler().postDelayed({
-      tv_hello_world.text = "Hello World"
+      tv_hello_world.text = getString(R.string.hello_world)
     }, 2000)
+    val answerToEverything = Constants.answerToEverything()
   }
 }
