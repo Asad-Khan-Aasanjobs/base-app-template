9d974e938be4e0553be21f6391e28ebb5f7f552e
AsadK <asad.khan@aasaanjobs.com>
2019-04-05 13:16:23 +0530

    Create new base_module as intermediate module
    * Provides the link between app module and global module
    * Hold code common  to different features
    * Liable to change so it's not in global_module
    * Not going to change too frequently so not in base_app module

---
+++ base_module/src/main/java/com/asadkhan/base_module/BaseConstant.kt
@@ -0,0 +1,14 @@
+package com.asadkhan.base_module
+
+import com.asadkhan.global.Constants
+import com.asadkhan.global.Constants.answerToEverything
+
+object BaseConstant {
+  
+  private const val CONSTANT_NUMBER = 42
+  
+  fun isAnswerRight(): Boolean {
+    return answerToEverything() == CONSTANT_NUMBER
+  }
+  
+}
